18d95c2c01a5e0cb02542188f5eaf156a4d5b8da
cpak125 <chris.pak125@gmail.com>
2022-05-23 16:17:35 -0400

    setup google oauth

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.redirect('/movies');
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    // Optionally force pick account every time
+    prompt: 'select_account'
+  }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/movies',
+    failureRedirect: '/movies'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/movies');
 });
 
 module.exports = router;
